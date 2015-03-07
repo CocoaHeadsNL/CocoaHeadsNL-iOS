bd3c37f4db780a4a10b10f43ef90cf7455c2ed3a
Bart Hoffman <info@barthoffman.nl>
2015-03-07 17:05:37 +0100

    Did a very basic setup for a tabbar app

---
+++ CocoaHeadsNL/CocoaHeadsNL/JobsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  JobsViewController.swift
+//  CocoaHeadsNL
+//
+//  Created by Bart Hoffman on 07/03/15.
+//  Copyright (c) 2015 Stichting CocoaheadsNL. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class JobsViewController: UICollectionViewController
+{
+    
+}
---
+++ CocoaHeadsNL/CocoaHeadsNL/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  CocoaHeadsNL
+//
+//  Created by Bart Hoffman on 07/03/15.
+//  Copyright (c) 2015 Stichting CocoaheadsNL. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController
+{
+    @IBOutlet weak var mapView: MKMapView!
+    
+}
