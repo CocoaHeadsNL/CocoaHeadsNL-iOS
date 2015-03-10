67da0db623880c081b79232ef4b8466ec0861f1d
Bart Hoffman <info@barthoffman.nl>
2015-03-10 17:04:30 +0100

    Added LocationListViewController (UIcollectionView) which should point to company details with Apps

---
+++ CocoaHeadsNL/CocoaHeadsNL/LocationListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LocationListViewController.swift
+//  CocoaHeadsNL
+//
+//  Created by Bart Hoffman on 10/03/15.
+//  Copyright (c) 2015 Stichting CocoaheadsNL. All rights reserved.
+//
+
+import Foundation
+
+class LocationListViewController: UICollectionViewController {
+    
+}
