43f00a06a32268c07b08a23a6ab073e5e14c4986
Bart Hoffman <info@barthoffman.nl>
2015-03-11 20:05:47 +0100

    Implemented basic setup for UICollectionViews

--- CocoaHeadsNL/CocoaHeadsNL/JobsViewController.swift
+++ CocoaHeadsNL/CocoaHeadsNL/JobsViewController.swift
@@ -11,5 +11,42 @@ import UIKit
 
 class JobsViewController: UICollectionViewController
 {
+    var cellIdentifier = "jobsCollectionViewCellIdentifier"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.registerClass(NSClassFromString("UICollectionViewCell"), forCellWithReuseIdentifier: cellIdentifier)
+
+    }
+    
+    
+    //MARK - UICollectionViewDataSource methods
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+    {
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath) as UICollectionViewCell
+        cell.backgroundColor = UIColor.grayColor()
+        
+        return cell
+    }
+    
+    //MARK - UICollectionViewDelegate methods
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        println("where is the job?")
+    }
+    
+    //MARK - UICollectionViewDelegateFlowLayout methods
+    
+    func collectionView(collectionView : UICollectionView,layout collectionViewLayout:UICollectionViewLayout,sizeForItemAtIndexPath indexPath:NSIndexPath) -> CGSize
+    {
+        return CGSizeMake(300, 120)
+    }
+    
     
 }
--- CocoaHeadsNL/CocoaHeadsNL/LocationListViewController.swift
+++ CocoaHeadsNL/CocoaHeadsNL/LocationListViewController.swift
@@ -10,4 +10,40 @@ import Foundation
 
 class LocationListViewController: UICollectionViewController {
     
+    var cellIdentifier = "companyCollectionViewCellIdentifier"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.registerClass(NSClassFromString("UICollectionViewCell"), forCellWithReuseIdentifier: cellIdentifier)
+        
+    }
+    
+    
+    //MARK - UICollectionViewDataSource methods
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+    {
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath) as UICollectionViewCell
+        cell.backgroundColor = UIColor.grayColor()
+        
+        return cell
+    }
+    
+    //MARK - UICollectionViewDelegate methods
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        println("where is that company?")
+    }
+    
+    //MARK - UICollectionViewDelegateFlowLayout methods
+    
+    func collectionView(collectionView : UICollectionView,layout collectionViewLayout:UICollectionViewLayout,sizeForItemAtIndexPath indexPath:NSIndexPath) -> CGSize
+    {
+        return CGSizeMake(300, 120)
+    }
 }
