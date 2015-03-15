accda3230d57c63c6b2f897f700c1b928a911bca
Bart Hoffman <info@barthoffman.nl>
2015-03-15 01:47:51 +0100

    added for testing purposes

---
+++ CocoaHeadsNL/CocoaHeadsNL/TabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarController.swift
+//  CocoaHeadsNL
+//
+//  Created by Bart Hoffman on 15/03/15.
+//  Copyright (c) 2015 Stichting CocoaheadsNL. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TabBarController: UITabBarController, UITabBarControllerDelegate
+{
+
+}
