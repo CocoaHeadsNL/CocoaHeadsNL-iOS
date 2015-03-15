fcba6d4efc9ad0c6bd33ba920f658ee5f5cf1376
Bart Hoffman <info@barthoffman.nl>
2015-03-15 01:50:22 +0100

    Added to set navBar on DetailTableViewController

---
+++ CocoaHeadsNL/CocoaHeadsNL/DetailNavigationController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailNavigationController.swift
+//  CocoaHeadsNL
+//
+//  Created by Bart Hoffman on 15/03/15.
+//  Copyright (c) 2015 Stichting CocoaheadsNL. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailNavigationController: UINavigationController {
+    
+}
