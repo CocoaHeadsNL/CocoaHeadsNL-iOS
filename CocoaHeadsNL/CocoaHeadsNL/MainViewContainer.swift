21496ec2e7ac3c7ba8107257e0e3bdee3b0923ea
Bart Hoffman <info@barthoffman.nl>
2015-03-16 09:49:28 +0100

    added MainViewContainer

---
+++ CocoaHeadsNL/CocoaHeadsNL/MainViewContainer.swift
@@ -0,0 +1,9 @@
+//
+//  TabBarContainer.swift
+//  CocoaHeadsNL
+//
+//  Created by Bart Hoffman on 16/03/15.
+//  Copyright (c) 2015 Stichting CocoaheadsNL. All rights reserved.
+//
+
+import Foundation
