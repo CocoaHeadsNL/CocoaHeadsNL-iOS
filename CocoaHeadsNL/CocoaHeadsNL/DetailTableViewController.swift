66dee1c36a16df77247a48328444ece62123c102
Bart Hoffman <info@barthoffman.nl>
2015-05-16 22:20:19 +0200

    Added affLinks to tableView and selection goes to Appstore. Need to fix getting separate apps

--- CocoaHeadsNL/CocoaHeadsNL/DetailTableViewController.swift
+++ CocoaHeadsNL/CocoaHeadsNL/DetailTableViewController.swift
@@ -123,8 +123,67 @@ class DetailTableViewController: UITableViewController, UITableViewDataSource, U
             if indexPath.section == 1 {
                 //section 1 = company apps
                 //need to return AffilitateCell
+                let cellId = "affiliateCell"
+                var cell = tableView.dequeueReusableCellWithIdentifier(cellId) as? PFTableViewCell
+                if cell == nil {
+                    cell = PFTableViewCell(style: .Subtitle, reuseIdentifier: cellId)
+                }
 
-               
+                if let cell = cell {
+                    if let affiliateLink = companyApps.objectAtIndex(indexPath.row) as? AffiliateLink {
+                        if let textLabel = cell.textLabel {
+                            textLabel.adjustsFontSizeToFitWidth = true
+                            textLabel.text = affiliateLink.productName
+                        }
+                        if let detailTextLabel = cell.detailTextLabel {
+                            detailTextLabel.text = affiliateLink.productCreator
+                        }
+                        if let imageView = cell.imageView {
+                            if let affiliateId = affiliateLink.affiliateId {
+                                imageView.image = UIImage(named: "CocoaHeadsNLLogo")
+                                imageView.contentMode = .ScaleAspectFit
+                                
+                                if let url = NSURL(string: "https://itunes.apple.com/lookup?id=\(affiliateId)") {
+                                    var request = NSURLRequest(URL: url)
+                                    let dataTask = NSURLSession.sharedSession().dataTaskWithRequest(request,
+                                        completionHandler: { [weak cell](data, response, error) -> Void in
+                                            var parseError: NSError?
+                                            let parsedObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data,
+                                                options: NSJSONReadingOptions.AllowFragments,
+                                                error:&parseError)
+                                            if let root = parsedObject as? NSDictionary {
+                                                if let results = root["results"] as? NSArray {
+                                                    if let result = results[0] as? NSDictionary {
+                                                        if let iconUrlString = result["artworkUrl100"] as? String {
+                                                            if let url = NSURL(string: iconUrlString) {
+                                                                var request = NSURLRequest(URL: url)
+                                                                let dataTask = NSURLSession.sharedSession().dataTaskWithRequest(request,
+                                                                    completionHandler: { (data, response, error) -> Void in
+                                                                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                                                                            if let cell = cell {
+                                                                                let image = UIImage(data: data)
+                                                                                imageView.image = image
+                                                                                cell.setNeedsLayout()
+                                                                            }
+                                                                        })
+                                                                })
+                                                                dataTask.resume()
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        })
+                                    dataTask.resume()
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                return cell!
+
+
             } else {
                 if actualRow > 0 {
                     // no map
@@ -195,8 +254,27 @@ class DetailTableViewController: UITableViewController, UITableViewDataSource, U
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        if tableView.indexPathForSelectedRow()?.section == 1 {
-             println("Should go to the appstore now")
+        if indexPath.section == 1 {
+            
+            if let affiliateToken = PFConfig.currentConfig()["appleAffiliateToken"] as? String {
+                if let affiliateLink = companyApps.objectAtIndex(indexPath.row) as? AffiliateLink {
+                    if let affiliateId = affiliateLink.affiliateId {
+                        if let url = NSURL(string: NSString(format: "https://itunes.apple.com/app/apple-store/id%@?at=%@&ct=app", affiliateId,
+                            affiliateToken) as String) {
+                                if UIApplication.sharedApplication().canOpenURL(url) {
+                                    if TARGET_IPHONE_SIMULATOR == 1 {
+                                        // No app store on simulator.
+                                        println("Actual device would open: \(url)")
+                                    } else {
+                                        UIApplication.sharedApplication().openURL(url)
+                                    }
+                                }
+                        }
+                    }
+                }
+            }
+            
+            self.tableView.deselectRowAtIndexPath(indexPath, animated: true)
         }
     }
 
