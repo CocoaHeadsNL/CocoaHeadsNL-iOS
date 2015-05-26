9f24668479fec42d4fc8b8cbc8a80375bfa7b141
Matthijs Hollemans <mail@hollance.com>
2015-05-26 20:40:44 +0200

    Use calendar metaphor.

--- CocoaHeadsNL/CocoaHeadsNL/MeetupCell.swift
+++ CocoaHeadsNL/CocoaHeadsNL/MeetupCell.swift
@@ -22,28 +22,12 @@ class MeetupCell: PFTableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var logoImageView: PFImageView!
+    @IBOutlet weak var separatorView: UIView!
     @IBOutlet weak var dateContainer: UIView!
+    @IBOutlet weak var calendarImageView: UIImageView!
     @IBOutlet weak var dayLabel: UILabel!
     @IBOutlet weak var monthLabel: UILabel!
     @IBOutlet weak var widthConstraint: NSLayoutConstraint!
-
-    let circleLayer: CAShapeLayer = {
-        let circleLayer = CAShapeLayer()
-        circleLayer.contentsScale = UIScreen.mainScreen().scale
-        circleLayer.position = CGPoint(x: 8, y: 12)
-        circleLayer.path = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: 48, height: 48), cornerRadius: 6).CGPath
-        circleLayer.lineWidth = 2
-        return circleLayer
-    }()
-
-    let trackLayer: CAShapeLayer = {
-        let trackLayer = CAShapeLayer()
-        trackLayer.contentsScale = UIScreen.mainScreen().scale
-        trackLayer.position = CGPoint(x: 8 + 48/2 - 1, y: 0)
-        trackLayer.strokeColor = UIColor.clearColor().CGColor
-        trackLayer.fillColor = UIColor.blackColor().CGColor
-        return trackLayer
-    }()
 
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -51,8 +35,6 @@ class MeetupCell: PFTableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        self.layer.insertSublayer(circleLayer, atIndex: 0)
-        self.layer.insertSublayer(trackLayer, atIndex: 0)
     }
 
     override func prepareForReuse() {
@@ -63,6 +45,7 @@ class MeetupCell: PFTableViewCell {
         dayLabel.text = ""
         monthLabel.text = ""
 
+        separatorView.hidden = false
         logoImageView.file = nil
 
         if let image = UIImage(named: "MeetupPlaceholder") {
@@ -74,13 +57,9 @@ class MeetupCell: PFTableViewCell {
     func configureCellForMeetup(meetup: Meetup, row: Int) {
         titleLabel.text = meetup.name
 
-        CATransaction.begin()
-        CATransaction.setDisableActions(true)
-
         if let date = meetup.time {
             let timeText = MeetupCell.dateFormatter.stringFromDate(date)
-            //timeLabel.text = String(format: "%@ - %@", timeText, meetup.locationName ?? "Location unknown")
-            timeLabel.text = timeText
+            timeLabel.text = String(format: "%@ - %@", timeText, meetup.locationName ?? "Location unknown")
 
             let components = NSCalendar.currentCalendar().components(.CalendarUnitMonth | .CalendarUnitDay, fromDate: date)
             dayLabel.text = String(format: "%d", components.day)
@@ -89,25 +68,13 @@ class MeetupCell: PFTableViewCell {
             monthLabel.text = months[components.month - 1]
 
             if date.timeIntervalSinceNow > 0 {
-                circleLayer.strokeColor = UIColor.blackColor().CGColor
-                circleLayer.fillColor = UIColor.whiteColor().CGColor
                 dayLabel.textColor = UIColor.blackColor()
-                monthLabel.textColor = UIColor.blackColor()
+                calendarImageView.image = UIImage(named: "CalendarFuture")
             } else {
-                circleLayer.strokeColor = UIColor.blackColor().CGColor
-                circleLayer.fillColor = UIColor.blackColor().CGColor
-                dayLabel.textColor = UIColor.whiteColor()
-                monthLabel.textColor = UIColor.whiteColor()
+                dayLabel.textColor = UIColor(white: 0, alpha: 0.65)
+                calendarImageView.image = UIImage(named: "CalendarPast")
             }
         }
-
-        if row == 0 {
-            trackLayer.path = UIBezierPath(rect: CGRect(x: 0, y: 20, width: 2, height: 132 - 20)).CGPath
-        } else {
-            trackLayer.path = UIBezierPath(rect: CGRect(x: 0, y: 0, width: 2, height: 132)).CGPath
-        }
-
-        CATransaction.commit()
 
         // Loading large images and resizing them is pretty inefficient.
         // It would be better if the server already gave us a -- transparent -- 
