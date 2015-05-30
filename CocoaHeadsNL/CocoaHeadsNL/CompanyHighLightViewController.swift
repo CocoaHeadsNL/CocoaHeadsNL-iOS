3830a739beccb846b5ec070c26bb55a1fded7a60
Bart Hoffman <info@barthoffman.nl>
2015-05-30 22:40:40 +0200

    added .swiftFiles corresponding to StoryBoardNaming in Project

---
+++ CocoaHeadsNL/CocoaHeadsNL/CompanyHighLightViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CompanyHighLightViewController.swift
+//  CocoaHeadsNL
+//
+//  Created by Bart Hoffman on 30/05/15.
+//  Copyright (c) 2015 Stichting CocoaheadsNL. All rights reserved.
+//
+
+import Foundation
