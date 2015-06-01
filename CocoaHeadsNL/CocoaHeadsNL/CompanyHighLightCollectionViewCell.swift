83a93a4c61f7bcd2479dbcb3f3edcc46be1e56b5
Bart Hoffman <info@barthoffman.nl>
2015-06-02 00:03:32 +0200

    initial setup for HighLightedCompany - non scrollable collectionView, want to add timer to show next company

---
+++ CocoaHeadsNL/CocoaHeadsNL/CompanyHighLightCollectionViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  CompanyHighLightCollectionViewCell.swift
+//  CocoaHeadsNL
+//
+//  Created by Bart Hoffman on 01/06/15.
+//  Copyright (c) 2015 Stichting CocoaheadsNL. All rights reserved.
+//
+
+import Foundation
+
+class CompanyHighLightCollectionViewCell: PFCollectionViewCell {
+    
+    override func updateFromObject(object: PFObject?) {
+        
+        if let company = object as? Company {
+            
+//            if let companyLogo = company.logo {
+//                imageView.file = companyLogo
+//                imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 89)
+//                imageView.image = UIImage(named: "CocoaHeadsNLLogo")
+//                imageView.loadInBackground().continueWithSuccessBlock({[weak self] (task: BFTask!) -> AnyObject! in
+//                    self?.setNeedsLayout()
+//                    return nil
+//                    })
+//            }
+            
+            if let place = company.place {
+                textLabel.text = place
+                textLabel.font = UIFont.systemFontOfSize(8)
+                textLabel.textAlignment = .Center
+            }
+            
+            contentView.layer.borderWidth = (2.0 / UIScreen.mainScreen().scale) / 2
+            contentView.layer.borderColor = UIColor.grayColor().CGColor
+        }
+    }
+    
+}
--- CocoaHeadsNL/CocoaHeadsNL/CompanyHighLightViewController.swift
+++ CocoaHeadsNL/CocoaHeadsNL/CompanyHighLightViewController.swift
@@ -7,3 +7,64 @@
 //
 
 import Foundation
+
+class CompanyHighLightViewController: PFQueryCollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.collectionView?.registerClass(CompanyHighLightCollectionViewCell.self, forCellWithReuseIdentifier: "companyHighLightCollectionViewCell")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.itemSize = CGSize(width: self.view.frame.size.width, height: 60)
+        }
+        
+    }
+    
+    
+    //MARK: - UICollectionViewDataSource methods
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath, object: PFObject?) -> PFCollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("companyHighLightCollectionViewCell", forIndexPath: indexPath) as! CompanyHighLightCollectionViewCell
+        
+        if let company = object as? Company {
+            cell.updateFromObject(company)
+        }
+        
+        return cell
+    }
+    
+    //MARK: - UICollectionViewDelegate methods
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        self.performSegueWithIdentifier("ShowDetail", sender: collectionView.cellForItemAtIndexPath(indexPath))
+    }
+    
+    //MARK: - Segues
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "ShowDetail" {
+            if let indexPath = self.collectionView?.indexPathForCell(sender as! UICollectionViewCell) {
+                let company = self.objectAtIndexPath(indexPath) as! Company
+                let dataSource = CompanyDataSource(object: company)
+                dataSource.fetchAffiliateLinks()
+                
+                let detailViewController = segue.destinationViewController as! DetailViewController
+                detailViewController.dataSource = dataSource
+            }
+        }
+    }
+    
+    //MARK: -Query
+    
+    override func queryForCollection() -> PFQuery {
+        let query = Company.query()
+        return query!.orderByAscending("place")
+    }
+    
+}
