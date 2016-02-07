13a741dc43dba1e7ad4b70c81681e96da467a94b
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2016-02-07 14:44:24 +0100

    Nest sync of contributors into it's own promise

--- CocoaHeadsNL/CloudKitSync/index.js
+++ CocoaHeadsNL/CloudKitSync/index.js
@@ -38,56 +38,62 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
   var container = CloudKit.getDefaultContainer();
   var database = container.publicCloudDatabase; // We'll only make calls to the public database.
 
-  // Sign in using the keyID and public key file.
-  container.setUpAuth()
-    .then(function(userInfo){
-      
-      //Load contributors from iCloud
-      var cloudKitFetchPromise = database.performQuery({ recordType: 'Contributor' }).then(function(response) {
-        return Promise.resolve(response.records)
-      })
-      //Load contributors from Github
-      var githubFetchPromise =  contributorsLoader.load().then(function(contributors) {
-        return Promise.resolve(contributors)
-      })
-      
-      return Promise.all([cloudKitFetchPromise, githubFetchPromise])
-      
-    }).then(contributors => {
-      var cloudKitContributors = contributors[0];
-      var githubContributors = contributors[1];
-      
-      var mappedGithubRecords = githubContributors.map(function(gitHubRecord) {
-        return {recordType: 'Contributor',
-          fields: {
-            contributor_id: {value: gitHubRecord["id"]},
-            avatar_url: {value: gitHubRecord["avatar_url"]},
-            name: {value: gitHubRecord["name"]},
-            commit_count: {value: gitHubRecord["commit_count"]},
-            url: {value: gitHubRecord["html_url"]}
-          }
-        }
-      })
-      
-      for (var mappedGithubRecord of mappedGithubRecords) {
-        var contributorId = mappedGithubRecord["fields"]["contributor_id"]["value"]
-        var filteredCloudRecords = cloudKitContributors.filter(function(cloudKitRecord) {
-          var cloudKitContributorId = cloudKitRecord.fields.contributor_id
-          if (cloudKitContributorId === undefined) {return false}
-          return (contributorId === cloudKitContributorId.value)
-        })
 
-        for (var filteredCloudRecord of filteredCloudRecords) {
-          if(filteredCloudRecord.recordChangeTag) {
-            mappedGithubRecord.recordChangeTag = filteredCloudRecords[0].recordChangeTag;
-          }
-          if(filteredCloudRecord.recordName) {
-            mappedGithubRecord.recordName = filteredCloudRecords[0].recordName;
-          }
-        }
-      }
+// Sign in using the keyID and public key file.
+container.setUpAuth()
+  .then(function(userInfo){
+    
+      var syncContributorsPromise = new Promise(function(resolve, reject) {
+        //Load contributors from iCloud
+        var cloudKitFetchPromise = database.performQuery({ recordType: 'Contributor' }).then(function(response) {
+          return Promise.resolve(response.records)
+        })
+        //Load contributors from Github
+        var githubFetchPromise =  contributorsLoader.load().then(function(contributors) {
+          return Promise.resolve(contributors)
+        })
       
-      return database.saveRecords(mappedGithubRecords);
+        return Promise.all([cloudKitFetchPromise, githubFetchPromise]).then(contributors => {
+          var cloudKitContributors = contributors[0];
+          var githubContributors = contributors[1];
+    
+          var mappedGithubRecords = githubContributors.map(function(gitHubRecord) {
+            return {recordType: 'Contributor',
+              fields: {
+                contributor_id: {value: gitHubRecord["id"]},
+                avatar_url: {value: gitHubRecord["avatar_url"]},
+                name: {value: gitHubRecord["name"]},
+                commit_count: {value: gitHubRecord["commit_count"]},
+                url: {value: gitHubRecord["html_url"]}
+              }
+            }
+          })
+    
+          for (var mappedGithubRecord of mappedGithubRecords) {
+            var contributorId = mappedGithubRecord["fields"]["contributor_id"]["value"]
+            var filteredCloudRecords = cloudKitContributors.filter(function(cloudKitRecord) {
+              var cloudKitContributorId = cloudKitRecord.fields.contributor_id
+              if (cloudKitContributorId === undefined) {return false}
+              return (contributorId === cloudKitContributorId.value)
+            })
+
+            for (var filteredCloudRecord of filteredCloudRecords) {
+              if(filteredCloudRecord.recordChangeTag) {
+                mappedGithubRecord.recordChangeTag = filteredCloudRecords[0].recordChangeTag;
+              }
+              if(filteredCloudRecord.recordName) {
+                mappedGithubRecord.recordName = filteredCloudRecords[0].recordName;
+              }
+            }
+          }
+    
+          return database.saveRecords(mappedGithubRecords);
+        }).then(function(response) {
+          resolve(response)
+        })
+      })
+
+      return syncContributorsPromise
     }).then(function(response) {
       console.log("Done");
       process.exit();
