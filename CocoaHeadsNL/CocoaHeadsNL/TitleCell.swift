1f9ff94c9e356021537d7da2cf7b52f4d2c557a6
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2016-05-22 20:10:47 +0200

    Reuse dateformatters.

--- CocoaHeadsNL/CocoaHeadsNL/TitleCell.swift
+++ CocoaHeadsNL/CocoaHeadsNL/TitleCell.swift
@@ -9,16 +9,26 @@ class TitleCell: UITableViewCell {
         }
     }
 
+    lazy var dateFormatter: NSDateFormatter = {
+        let dateFormatter = NSDateFormatter()
+        let dateFormat = NSDateFormatter.dateFormatFromTemplate("MMMMd",
+                                                                options: 0,
+                                                                locale: NSLocale.currentLocale())
+        dateFormatter.dateFormat = dateFormat
+        return dateFormatter
+    }()
+
+    lazy var timeFormatter: NSDateFormatter = {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.timeStyle = .ShortStyle
+        return dateFormatter
+    }()
+
     var date: NSDate? {
         didSet {
             if let date = date {
-                let dateFormatter = NSDateFormatter()
-                let dateFormat = NSDateFormatter.dateFormatFromTemplate("MMMMd", options: 0, locale: NSLocale.currentLocale())
-                dateFormatter.dateFormat = dateFormat
-
                 let dateString = dateFormatter.stringFromDate(date)
-                dateFormatter.timeStyle = .ShortStyle
-                let timeString = dateFormatter.stringFromDate(date)
+                let timeString = timeFormatter.stringFromDate(date)
                 self.titleLabel.text = dateString + ", " + timeString
             } else {
                 self.titleLabel.text = "-"
