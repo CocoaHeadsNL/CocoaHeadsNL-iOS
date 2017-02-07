50ede1b59cee915fb86b88e2d8ebb235d4b4496f
Bart Hoffman <bart.hoffman@gmail.com>
2017-02-07 19:47:08 +0100

    removed old viewController from companiesTab

--- CocoaHeadsNL/CocoaHeadsNL/LocatedCompaniesViewController.swift
+++ CocoaHeadsNL/CocoaHeadsNL/LocatedCompaniesViewController.swift
@@ -8,17 +8,66 @@
 
 import Foundation
 import UIKit
+import CloudKit
 import Crashlytics
+import RealmSwift
 
 class LocatedCompaniesViewController: UITableViewController {
+    
+    let realm = try! Realm()
+    
+    var companiesArray = try! Realm().objects(Company.self).sorted(byKeyPath: "name")
+    
+    var placesArray: [String] {
+        get {
+            var places = Set<String>()
+            places.formUnion(companiesArray.flatMap { $0.place })
+            return places.map{ $0 }.sorted()
+        }
+    }
 
-    var companiesArray = [Company]()
+    //var companiesArray = [Company]()
+    var notificationToken: NotificationToken?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let backItem = UIBarButtonItem(title: NSLocalizedString("Companies"), style: .plain, target: nil, action: nil)
         self.navigationItem.backBarButtonItem = backItem
+        
+        self.navigationItem.titleView = UIImageView(image: UIImage(named: "Banner")!)
+        
+        self.subscribe()
+        
+        // Set results notification block
+        self.notificationToken = companiesArray.addNotificationBlock { (changes: RealmCollectionChange) in
+            switch changes {
+            case .initial:
+                // Results are now populated and can be accessed without blocking the UI
+                self.tableView.reloadData()
+                break
+            case .update(_, _, _, _):
+                self.tableView.reloadData()
+                break
+            case .error(let err):
+                // An error occurred while opening the Realm file on the background worker thread
+                fatalError("\(err)")
+                break
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        Answers.logContentView(withName: "Show companies",
+                               contentType: "Company",
+                               contentId: "overview",
+                               customAttributes: nil)
     }
 
     //MARK: - Segues
@@ -42,11 +91,33 @@ class LocatedCompaniesViewController: UITableViewController {
             }
         }
     }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "ShowCompanies" {
+//            if let indexPath = self.tableView.indexPath(for: sender as! UITableViewCell) {
+//                
+//                let detailViewController = segue.destination as? LocatedCompaniesViewController
+//                
+//                let place = placesArray[(indexPath as NSIndexPath).row]
+//                
+//                detailViewController?.companiesArray = companiesArray.filter({ $0.place == place })
+//                
+//                Answers.logContentView(withName: "Show company location",
+//                                       contentType: "Company",
+//                                       contentId: placesArray[(indexPath as NSIndexPath).row],
+//                                       customAttributes: nil)
+//            }
+//        }
+//    }
 
     //MARK: - UITableViewDelegate
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return placesArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return NSLocalizedString(placesArray[section])
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -75,5 +146,27 @@ class LocatedCompaniesViewController: UITableViewController {
         self.tableView.deselectRow(at: indexPath, animated: true)
         self.performSegue(withIdentifier: "ShowDetail", sender: tableView.cellForRow(at: indexPath))
     }
+    
+    //MARK: - Notifications
+    
+    func subscribe() {
+        let publicDB = CKContainer.default().publicCloudDatabase
+        
+        let subscription = CKSubscription(
+            recordType: "Companies",
+            predicate: NSPredicate(value: true),
+            options: .firesOnRecordCreation
+        )
+        
+        let info = CKNotificationInfo()
+        
+        info.alertBody = NSLocalizedString("A new company has been added!")
+        info.shouldBadge = true
+        
+        subscription.notificationInfo = info
+        
+        publicDB.save(subscription, completionHandler: { record, error in })
+    }
+
 
 }
