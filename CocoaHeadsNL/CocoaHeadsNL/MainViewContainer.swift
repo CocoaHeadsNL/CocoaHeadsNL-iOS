35df082b600af1cc7b267521ff80d57aad3f8fad
Said Ozcan <said.ozcan@booking.com>
2017-06-19 23:13:19 +0200

    delete unused files
    This commit deletes two unused files

--- CocoaHeadsNL/CocoaHeadsNL/MainViewContainer.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TabBarContainer.swift
-//  CocoaHeadsNL
-//
-//  Created by Bart Hoffman on 16/03/15.
-//  Copyright (c) 2016 Stichting CocoaheadsNL. All rights reserved.
-//
-
-import Foundation
